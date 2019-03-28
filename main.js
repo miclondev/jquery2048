036a55485d7ce34fe03be0c09af030e305b90e2e
mic lon <miclondev@gmail.com>
2019-03-28 19:18:13 +0300

    second

--- main.js
+++ main.js
@@ -1,90 +1,96 @@
 $('document').ready(function () {
-
     //generate first random number between 1 and 16
     function randomNum() {
-        let num = Math.floor(Math.random() * 16) + 1
-        return num
+        return Math.floor(Math.random() * 16) + 1
     }
-
     //generate second random number but check to make sure its not the same as rando number 1
     //use recurssion to retry
+    let num1
+    let num2
 
-    function generateNum2(check) {
-        //generate num 2
-        var num2 = randomNum()
-        //check if num 2 i
-        if (num2 === check) {
-            console.log('is equal regenerating')
-            var num3 = randomNum()
-            return num3
+    function generateNum2() {
+        console.log('generating num 2')
+        num2 = randomNum()
+        if (num2 === num1) {
+            console.log('is same regenerating')
+            return generateNum2()
         }
         return num2;
     }
 
-    let num1 = randomNum()
-    let num2 = generateNum2(num1)
+    num1 = randomNum()
+    num2 = generateNum2()
 
-    //make sure num1 is not = to num 2 
+    let blocksArray = []
+    //set current count and reset array count
+    function resetBlockCount() {
+        blocksArray = []
+        for (let i = 0; i < 16; i++) {
+            blocksArray.push(0)
+        }
+    }
+
+    resetBlockCount()
+    console.log(blocksArray)
+    console.log(num1, num2)
+
+    //reassign random number
     function reassignRandom() {
         num1 = randomNum()
-        num2 = generateNum2(num1)
+        num2 = generateNum2()
     }
 
     //get all 16 blocks
     var blocks = $('.blocks');
-    //create an array with 1 to 16
-    var blocksArray = Array.from(Array(17).keys())
-    //function to remove a number
-    function removeFilled(num) {
-        var index = blocksArray.indexOf(num)
-        // console.log(index)
-        return blocksArray.splice(index, 1)
-    }
 
-    removeFilled(0)
+    blocks.each(function (b, i) {
+        var currentBlock = $(this)
+        var blockNum = currentBlock.attr('data-block')
+        console.log(blockNum)
 
-    console.log(blocksArray)
-    //find a way to keep track of filled and nums
-    //generate array of 16 objects with current number
-    //dont thing we need blocksArray now
-
-    let mainArr = []
-
-    blocksArray.forEach(function (num) {
-        mainArr.push({
-            num: num,
-            current: 0,
-            filled: false
-        })
     })
 
-    console.log(blocksArray)
-    //reassign blocks
-    function reassignBlocks() {
-        return mainArr.forEach(function (n) {
-            if (n.filled) {
-                blocks.each(function (i) {
-                    let blockNumber = $(this).attr('data-block')
-                    let currentBlock = $(this)
-                    console.log(typeof (blockNumber))
-                    if (n.num === parseInt(blockNumber)) {
-                        currentBlock.find('span').text('2')
-                        currentBlock.addClass('block-2')
-                    }
-                })
+    function assignNumber(num, count) {
+        let value = num - 1
+        blocksArray[value] = blocksArray[value] + count
+        console.log(blocksArray)
+    }
+
+    ///assign to array
+    function onAssignArray() {
+        blocksArray.forEach(function (b, i) {
+            if (b !== 0) {
+                console.log(b)
+                blocks.eq(parseInt(i)).addClass('block-2')
+                blocks.eq(i).find('span').text(b)
             }
         })
     }
 
 
+    function InitialassignClasses() {
+        blocks.each(function () {
+            var currentBlock = $(this)
+            var blockNum = currentBlock.attr('data-block')
+            if (parseInt(blockNum) === num1 || parseInt(blockNum) === num2) {
+                assignNumber(blockNum, 2)
+            }
+        })
+        onAssignArray()
+    }
+
+    InitialassignClasses()
+
+
+    //fill in the block with the random 
+
+
     $('#reset').click(function () {
-        console.log("reset red")
-        reassignRandom()
-        console.log(num1, num2)
-        resetCurrentArray()
+        console.log("reseting")
         clearBlocks()
-        putIntoBlock()
-        reassignBlocks()
+        resetBlockCount()
+        reassignRandom()
+        InitialassignClasses()
     })
 
 
@@ -96,81 +102,27 @@ $('document').ready(function () {
         })
     }
 
-    function putIntoBlock() {
-        return blocks.each(function (index) {
-            var blockNumber = $(this).attr('data-block')
-            if (parseInt(blockNumber) === num1 || parseInt(blockNumber) === num2) {
-                mainArr[blockNumber].current = 2
-                mainArr[blockNumber].filled = true
-            }
-        })
-    }
-
-    function initialAssign() {
-        return mainArr.forEach(function (num) {
-            if (num.num === num1 || num.num === num2) {
-                console.log('initial')
-                console.log(num.num, num1, num2)
-                num.current = 2
-                num.filled = true
-            }
-        })
-    }
-
-    function resetCurrentArray() {
-        let newArr = []
-        blocksArray.forEach(function (num) {
-            newArr.push({
-                num: num,
-                current: 0,
-                filled: false
-            })
-        })
-        mainArr = newArr
-    }
+    // blocksArray.forEach(function (b, i) {
+    //     console.log(i, 'modulus result', i % 4)
+    // })
 
     function onMoveRight() {
-        console.log(mainArr)
-        mainArr.forEach(function(b){
-            //console.log(b)
-            let nextnum = b.num + 1
+        blocksArray.forEach(function (b, i) {
+            if (b !== 0) {
+                if (b % 4 !== 0) {
+                    console.log(i+1)
+                    const nextBlockInArray = i + 1
+                    const currentCount = b
+                    const nextCount = blocksArray[nextBlockInArray]
 
-            if(b.current > 0){
-                b.current = 0
-                b.filled = false
-            }
-
-            if(nextnum === b.num){
-                b.current = 2
-                b.filled = true
+                   
+                    blocksArray[nextBlockInArray] = currentCount + nextCount 
+                    blocksArray[i] = 0
+                }
             }
         })
-        console.log(mainArr)
-     clearBlocks() 
-
-        // mainArr.forEach(function (b, i) {
-        //     console.log(b,i)
-        //     let modI = i + 1
-        //     if (modI > 4) {
-        //         modI = modI % 4
-        //         if (modI === 0) {
-        //             modI = 4
-        //         } 
-        //     }
-        //         if (b.filled === true) {
-        //             b.filled = false
-        //             b.current = 0 
-
-        //             let next = b.num + 1
-        //             console.log(b.num)
-        //             console.log(next, typeof (next), mainArr[next])
-        //             mainArr[next].filled = true
-        //             mainArr[next].current = 2
-        //         }
-        //         //console.log(nextnum, i)
-            
-        // })
-        reassignBlocks()
+        console.log(blocksArray)
+        onAssignArray()
     }
 
 
@@ -192,6 +144,6 @@ $('document').ready(function () {
     })
 
     //putIntoBlock()
-    initialAssign()
-    reassignBlocks()
+    // initialAssign()
+    // reassignBlocks()
 })
